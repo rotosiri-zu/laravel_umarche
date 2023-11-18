733e9fc8a541ff73020a03dfc1dec36fe5c55073
rotosiri-zu <hongjitufang25@gmail.com>
2023-11-18 21:51:00 +0900

    属性バック

--- resources/views/components/tests/app.blade.php
+++ resources/views/components/tests/app.blade.php
@@ -11,8 +11,11 @@
   <!-- Fonts -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
+  <!-- Styles -->
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
   <!-- Scripts -->
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <script src="{{ asset('js/app.js') }}" defer></script>
 </head>
 
 <body>
