cd63171544c42fbba85c1ec29cc5438d882d1889
rotosiri-zu <hongjitufang25@gmail.com>
2024-02-02 23:52:41 +0900

    image index

--- routes/owner.php
+++ routes/owner.php
@@ -9,7 +9,10 @@ use App\Http\Controllers\Owner\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Owner\Auth\RegisteredUserController;
 use App\Http\Controllers\Owner\Auth\VerifyEmailController;
 use App\Http\Controllers\Owner\ShopController;
+use App\Http\Controllers\Owner\ImageController;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -31,6 +34,9 @@ Route::prefix('shops')->middleware('auth:owners')->group(function () {
   Route::get('edit/{shop}', [ShopController::class, 'edit'])->name('shops.edit');
   Route::post('update/{shop}', [ShopController::class, 'update'])->name('shops.update');
 });
+
+Route::resource('images', ImageController::class)->middleware('auth:owners')->except(['show']);
+
 
 Route::get('/dashboard', function () {
   return view('owner.dashboard');
