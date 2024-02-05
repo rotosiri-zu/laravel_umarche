cffdd1bf4306ec8be54be79494afa2a1d4861f75
rotosiri-zu <hongjitufang25@gmail.com>
2024-02-05 21:51:55 +0900

    image delete

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -69,7 +69,7 @@
   <script>
     function deletePost(e) {
       'use strict';
-      if (confirm('ほんとうにさくじょしてもいいですか？')) {
+      if (confirm('本当に削除してもいいですか？')) {
         document.getElementById('delete_' + e.dataset.id).submit();
       }
     }
