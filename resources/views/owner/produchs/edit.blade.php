ce2f929365f056fe18f5030f5f74539c58fc87c1
rotosiri-zu <hongjitufang25@gmail.com>
2024-03-02 22:53:01 +0900

    product destroy

--- resources/views/owner/produchs/edit.blade.php
+++ resources/views/owner/produchs/edit.blade.php
@@ -102,6 +102,13 @@
               </div>
             </div>
           </form>
+          <form id="delete_{{$product->id}}" method="post" action="{{ route('owner.products.destroy', ['product' => $product->id]) }}">
+            @csrf
+            @method('delete')
+            <div class="p-2 w-full flex justify-around mt-32">
+              <a href="#" date-id="{{$product->id}}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-indigo-500 rounded">削除する</a>
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -122,5 +129,12 @@
         MicroModal.close(modal); // モーダルを閉じる
       })
     })
+
+    function deletePost(e) {
+      'use strict';
+      if (confirm('本当に削除してもいいですか？')) {
+        document.getElementById('delete_' + e.dataset.id).submit();
+      }
+    }
   </script>
 </x-app-layout>
