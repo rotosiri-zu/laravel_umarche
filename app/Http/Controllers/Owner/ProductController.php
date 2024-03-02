7f594ee78590b4129132e5daca3e2ce099ac0df5
rotosiri-zu <hongjitufang25@gmail.com>
2024-03-02 22:20:18 +0900

    product update

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -12,8 +12,8 @@ use App\Models\Stock;
 use App\Models\Shop;
 use App\Models\PrimaryCategory;
 use App\Models\Owner;
-
-
+use App\Http\Requests\ProductRequest;
+use Database\Seeders\ProductSeeder;
 
 class ProductController extends Controller
 {
@@ -74,22 +74,8 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        $request->validate([
-            'name' => 'required', 'string', 'max:50',
-            'information' => 'required', 'string', 'max:1000',
-            'price' => 'required', 'integer',
-            'sort_order' => 'nullable', 'integer',
-            'quantity' => 'required', 'integer',
-            'shop_id' => 'required', 'exists:shops,id',
-            'category' => 'required', 'exists:secondary_categories,id',
-            'image1' => 'nullable', 'exists:images,id',
-            'image2' => 'nullable', 'exists:images,id',
-            'image3' => 'nullable', 'exists:images,id',
-            'image4' => 'nullable', 'exists:images,id',
-            'is_selling' => 'required'
-        ]);
 
         try {
             DB::transaction(function () use ($request) {
@@ -150,16 +136,65 @@ class ProductController extends Controller
         );
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(ProductRequest $request, $id)
     {
-        //
+        $request->validate([
+            'current_quantity' => 'required|integer',
+        ]);
+
+        $product = Product::findOrFail($id);
+        $quantity = Stock::where('product_id', $product->id)
+            ->sum('quantity');
+
+        if ($request->current_quantity !== $quantity) {
+            $id = $request->route()->parameter('product');
+            return redirect()->route('owner.products.edit', ['product' => $id])
+                ->with([
+                    'message', '在庫数が変更されています。再度確認してください。',
+                    'status' => 'alert'
+                ]);
+        } else {
+            try {
+                DB::transaction(function () use ($request, $product) {
+
+                    $product->name = $request->name;
+                    $product->information = $request->information;
+                    $product->price = $request->price;
+                    $product->sort_order = $request->sort_order;
+                    $product->shop_id = $request->shop_id;
+                    $product->secondary_category_id = $request->category;
+                    $product->image1 = $request->image1;
+                    $product->image2 = $request->image2;
+                    $product->image3 = $request->image3;
+                    $product->image4 = $request->image4;
+                    $product->is_selling = $request->is_sellin;
+                    $product->save();
+
+                    if ($request->type === '1') {
+                        $newQuantity = $request->Quantity;
+                    }
+
+                    if ($request->type === '2') {
+                        $newQuantity = $request->Quantity * -1;
+                    }
+                    Stock::create([
+                        'product_id' => $product->id,
+                        'type' => $request->type,
+                        'quantity' => $newQuantity,
+                    ]);
+                }, 2);
+            } catch (Throwable $e) {
+                Log::error($e);
+                throw $e;
+            }
+
+            return redirect()
+                ->route('owners.products.index')
+                ->with([
+                    'message', '商品情報を更新しました。',
+                    'status' => 'info'
+                ]);
+        }
     }
 
     /**
